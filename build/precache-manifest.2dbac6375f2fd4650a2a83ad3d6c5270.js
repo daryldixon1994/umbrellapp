95766126a5ccd53b18d871892e8eafec335dc137
daryldixon1994 <kmohamedanouar@gmail.com>
2021-12-21 13:35:05 +0100

    add build

---
+++ build/precache-manifest.2dbac6375f2fd4650a2a83ad3d6c5270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c752142632c43577367d7b359079b9a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "affda29de4c5fbcb4d18",
+    "url": "/static/css/main.6179cbe8.chunk.css"
+  },
+  {
+    "revision": "1f42f71006e717269aab",
+    "url": "/static/js/2.3eef56f6.chunk.js"
+  },
+  {
+    "revision": "fe397b565d71d15c37a5e3bd0baaf41b",
+    "url": "/static/js/2.3eef56f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "affda29de4c5fbcb4d18",
+    "url": "/static/js/main.f55211d9.chunk.js"
+  },
+  {
+    "revision": "4ead754694b4ba11ed64",
+    "url": "/static/js/runtime-main.ed7faa24.js"
+  }
+]);
